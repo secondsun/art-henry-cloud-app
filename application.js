cffcf08c57db0ab566d5cac2c2e79103af386c76
testing-admin@example.com <testing-admin@example.com>
2015-11-04 04:09:06 +0000

    update file /application.js

--- application.js
+++ application.js
@@ -24,6 +24,7 @@ app.use(bodyParser());
 // Add extra routes here
 app.post('/account/login', account.login);
 app.get('/account/:accountId', account.getAccount);
+app.get('/account/me', account.getMe);
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
